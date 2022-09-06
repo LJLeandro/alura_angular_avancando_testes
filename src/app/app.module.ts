e9f3d183a2c2152417cefb1ad230a19323f30452
Lucas Leandro <lucasjleandro@gmail.com>
2022-09-06 10:47:02 -0300

    Primeiro Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LikeWidgetComponent } from './shared/components/like-widget/like-widget.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LikeWidgetModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
